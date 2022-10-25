cb183d4f05c2da1f91196cdecede1d69237f9ec4
GitHub-Leon <leon.pliessnig@outlook.com>
2022-10-25 15:40:01 +0200

    unify data
    unified coordinates
    outsourced polyline to main
    added ability to log both hands simultaneously

--- hand_tracking/hand_tracker.py
+++ hand_tracking/hand_tracker.py
@@ -20,7 +20,6 @@ class HandTracker:
                                          self.track_con)
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
-        self.data_points = []  # used for drawing index finger
         self.results = []
 
     def hands_finder(self, image, draw=True):
@@ -50,11 +49,11 @@ class HandTracker:
 
         return image
 
-    def position_finder(self, image, label="Right"):
+    def position_finder(self, image, label="None"):
         """
         function to find image coordinates of a certain hand
         :param image: used to get width and height
-        :param label:"Right" or "Left" allowed
+        :param label:"Right" or "Left" allowed, if both hands should be returned, default value "None" must be applied
         :return: landmark list of hand in form [landmark_id, x, y]
         """
         return_landmark_list = []
@@ -72,16 +71,9 @@ class HandTracker:
 
                 # iterate over all stored hand landmarks with hand type data
             for hand_landmarks, hand_type in zip(hand_landmarks_list, hands_type):
-                if hand_type == label:
-                    for lm in hand_landmarks.landmark:
+                if hand_type == label or label == "None":
+                    for landmark_id, lm in enumerate(hand_landmarks.landmark):
                         cx, cy = int(lm.x * w), int(lm.y * h)
-                        return_landmark_list.append([id, cx, cy])
-
-                    x = int(hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].x * w)
-                    y = int(hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].y * h)
-                    self.data_points.append((x, y))
-
-        # draw polyline
-        draw_polyline(self.data_points, image)
+                        return_landmark_list.append([hand_type, landmark_id, (cx, cy)])
 
         return return_landmark_list
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
 
+from hand_tracking.drawing.draw_on_image import draw_polyline
 from hand_tracking.hand_tracker import HandTracker
 
 
 def main():
     cap = cv2.VideoCapture(0)  # capture live webcam frames
     tracker = HandTracker()
+
+    test_data_points = []  # gets replaced with db prob
 
     while cap.isOpened:  # while we capture the video, analyse each frame
         success, image = cap.read()
@@ -15,7 +18,12 @@ def main():
             continue
 
         image = tracker.hands_finder(image)
-        landmark_list = tracker.position_finder(image)
+        landmark_list = tracker.position_finder(image, "Right")
+
+        # to draw polyline
+        if len(landmark_list) > 0:
+            test_data_points.append(landmark_list[8][2])
+            draw_polyline(landmark_list, image)
 
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow("Hand- und Fingerverfolgung", cv2.flip(image, 1))
