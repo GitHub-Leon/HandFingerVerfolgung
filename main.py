db46384361913f2685811fc125a9d27ce5dc8bcb
GitHub-Leon <70367593+GitHub-Leon@users.noreply.github.com>
2022-10-27 09:55:11 +0200

    fixed error
    fixed error to get correct data for landmarks

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     cap = cv2.VideoCapture(0)  # capture live webcam frames
     tracker = HandTracker()
 
-    test_data_points = []  # gets replaced with db prob
+    test_data_points = []  # TODO: gets replaced with db prob
 
     while cap.isOpened:  # while we capture the video, analyse each frame
         success, image = cap.read()
@@ -20,10 +20,14 @@ def main():
         image = tracker.hands_finder(image)
         landmark_list = tracker.position_finder(image, "Right")
 
-        # to draw polyline
-        if len(landmark_list) > 0:
+        # TODO: append data to local list to display landmark (gets replaced with DB)
+        try:
             test_data_points.append(landmark_list[8][2])
-            draw_polyline(landmark_list, image)
+        except IndexError:  # landmark moved out of screen
+            print("Landmark lost")
+
+        # to draw polyline
+        draw_polyline(test_data_points, image)
 
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow("Hand- und Fingerverfolgung", cv2.flip(image, 1))
