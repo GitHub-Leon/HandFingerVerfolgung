9e2c98aff0ae259032f5678cce63a1897a5a0d1d
GitHub-Leon <70367593+GitHub-Leon@users.noreply.github.com>
2022-10-30 14:56:00 +0100

    added a config file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import configparser
 
 from hand_tracking.drawing.draw_on_image import draw_polyline
 from hand_tracking.hand_tracker import HandTracker
@@ -9,8 +10,17 @@ def main():
     cap = cv2.VideoCapture(0)  # capture live webcam frames
     handTracker = HandTracker()
     objectTracker = ObjectTracker()
+    config = configparser.ConfigParser()
+
+    config.read('config.ini')
+    showImg = config['DEFAULT'].getboolean('showImg')
+    drawPolyLine = config['DEFAULT'].getboolean('drawPolyLine')
+    drawHandLandMarks = config['DEFAULT'].getboolean('drawHandLandMarks')
+    drawObjectDetection = config['DEFAULT'].getboolean('drawObjectDetection')
+    showDebugMessage = config['DEFAULT'].getboolean('showDebugMessages')
 
     test_data_points = []  # TODO: gets replaced with db prob
+
 
     while cap.isOpened:  # while we capture the video, analyse each frame
         success, image = cap.read()
@@ -19,25 +29,28 @@ def main():
             print("Ignoring empty frame.")
             continue
 
-        image = objectTracker.object_finder(cv2.flip(image, 1))  # flip image, to display selfie view
-        image = handTracker.hands_finder(image)
+        image = objectTracker.object_finder(cv2.flip(image, 1), drawObjectDetection)  # flip image, to display selfie view
+        image = handTracker.hands_finder(image, drawHandLandMarks)
         landmark_list = handTracker.position_finder(image, "Right")
 
 
         # TODO: append data to local list to display landmark (gets replaced with DB)
-        # try:
-        #     x, y, z = landmark_list[8][2]
-        #     test_data_points.append((x, y))
-        #     print(z)
-        # except IndexError:  # landmark moved out of screen
-        #     print("Landmark lost")
+        try:
+            x, y, z = landmark_list[8][2]
+            test_data_points.append((x, y))
+        except IndexError:  # landmark moved out of screen
+            if showDebugMessage:
+                print("Landmark lost")
 
 
-        # to draw polyline TODO: option to enable it
-        # draw_polyline(test_data_points, image)
+        # to draw polyline
+        if drawPolyLine:
+            draw_polyline(test_data_points, image)
 
         # Flip the image horizontally for a selfie-view display.
-        cv2.imshow("Hand- und Fingerverfolgung", image)
+        if showImg:
+            cv2.imshow("Hand- und Fingerverfolgung", image)
+
         cv2.waitKey(30)  # frame updates in ms
 
     cap.release()
