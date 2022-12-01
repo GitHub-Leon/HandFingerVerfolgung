07a191a7ae4801ad390bcddd4414cc949399ef2a
GitHub-Leon <70367593+GitHub-Leon@users.noreply.github.com>
2022-12-01 09:33:40 +0100

    added option to only plot a certain part of logged landmarks

--- hand_tracking/drawing/plot.py
+++ hand_tracking/drawing/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-amount_of_logs = 10000
+amount_of_logs = 600  # number of picture processes done, until a graph is plotted
 total_data = []
 
 
@@ -9,5 +9,7 @@ def plot_data(data):
     total_data.append(data)
 
     if len(total_data) == amount_of_logs:
-        plt.plot(np.arange(0, len(total_data)), np.array(total_data), 'o')
+        new_total_data = total_data[100:]
+        plt.plot(np.arange(0, len(new_total_data)), np.array(new_total_data), 'o')
+        plt.axhline(y=np.nanmean(np.array(new_total_data)), color='red', linewidth=2)
         plt.show()
