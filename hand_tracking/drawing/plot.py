67055e0a910f292fb98934dd755c56bab3cae352
GitHub-Leon <70367593+GitHub-Leon@users.noreply.github.com>
2022-12-01 10:16:25 +0100

    added moving average plotting functionality

--- hand_tracking/drawing/plot.py
+++ hand_tracking/drawing/plot.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-amount_of_logs = 600  # number of picture processes done, until a graph is plotted
+amount_of_logs = 200  # number of picture processes done, until a graph is plotted
+moving_average = 20  # moving average length
+
 total_data = []
+average_data = []
 
 
 def plot_data(data):
     total_data.append(data)
+    average_data.append(np.mean(np.array(total_data[-moving_average:])))
 
     if len(total_data) == amount_of_logs:
-        new_total_data = total_data[100:]
-        plt.plot(np.arange(0, len(new_total_data)), np.array(new_total_data), 'o')
-        plt.axhline(y=np.nanmean(np.array(new_total_data)), color='red', linewidth=2)
+        plt.plot(np.arange(0, len(total_data[100:])), np.array(total_data[100:]), 'o')
+        plt.plot(np.arange(0, len(average_data[100:])), np.array(average_data[100:]))  # used for graphing the moving average
+        # plt.axhline(y=np.nanmean(np.array(new_total_data)), color='red', linewidth=2)  # uncomment, if the average of the whole graph needs to be plotted
+
         plt.show()
